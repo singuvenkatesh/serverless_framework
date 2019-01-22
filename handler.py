61b0d270d913e5932aa4c2cb3c2e8ac8540eb583
singuvenkatesh <singuvenkatesh@gmail.com>
2019-01-22 15:56:51 +0530

    Update handler.py

--- handler.py
+++ handler.py
@@ -4,6 +4,6 @@ import json
 def hello(event, context):
     return dict(
         statusCode=200,
-        body="hello welcome to Demo on serverless Framework"
+        body="Hello !!!  welcome to Demo on serverless Framework"
     )
   
